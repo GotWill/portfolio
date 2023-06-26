c7ee53adf4bcdd951875e6e3c5c1af0fca2b4b5e
Willian Pereira <williansouza9687a@gmail.com>
2023-06-26 12:22:33 -0300

    ajustes slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -3,7 +3,7 @@ import IconBolt from "@/icons/iconBolt";
 export default function Slider(){
     return (
         <section className="bg-gradient-css pt-28 pb-20  border border-t-transparent border-l-transparent border-r-transparent border-b-secondary">
-            <div className="flex justify-center items-center gap-6 overflow-hidden">
+            <div className="flex justify-center items-center gap-6 overflow-hidden w-full">
                 <h6 className="flex justify-center items-center gap-6 font-bold text-primary  animate-slider-text whitespace-nowrap ">
                     Front-End Developer
                     <IconBolt width={25} height={25} className="text-primary"/>
