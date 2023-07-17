8528a272378c3a682bc4a55d9c45ee323a4570a8
Willian Pereira <williansouza9687a@gmail.com>
2023-07-17 14:50:36 -0300

    add new skils

--- src/components/SkilsTec.tsx
+++ src/components/SkilsTec.tsx
@@ -15,13 +15,13 @@ export default function SkilsTec() {
                     itemsSkilTec.map((skil) => {
                         console.log(skil.bg)
                         return (
-                            <div key={skil.id} className="w-48 h-40 px-3 bg-secondary rounded-3xl flex justify-center items-center flex-col gap-4  overflow-hidden text-white relative">
+                            <div key={skil.id} className="w-48 h-40 md-mobile:w-40 px-3 bg-secondary rounded-3xl flex justify-center items-center flex-col gap-4  overflow-hidden text-white relative">
 
                                 <div className="z-10">
                                     {skil.imagePath}
                                 </div>
 
-                                <h4 className="text-base font-bold z-10">{skil.title}</h4>
+                                <h4 className="text-base font-bold z-10 text-center">{skil.title}</h4>
 
                                 <span className={`border-after before:absolute before:left-[4.5rem] before:animate-spin-slow before:w-20 before:h-64 before:bg-btn1 before:top-[-40%] after:absolute after:inset-[5px] bg-gradient-css after:rounded-3xl`}></span>
                             </div>
